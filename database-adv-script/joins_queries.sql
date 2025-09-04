9a0f5df6de3515648554ef6ee6ec3947575f31ea
joe <joe404prodev@gmail.com>
2025-09-04 15:43:07 +0300

    final

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,13 +1,13 @@
 SELECT *
 FROM Booking
 INNER JOIN User
-ON Booking.user_id = User.id
+ON Booking.user_id = User.id;
 
 SELECT *
 FROM Property
 LEFT JOIN Review
 ON Property.id = Review.property_id
-
+ORDER BY Property.id;
 
 SELECT *
 FROM User
